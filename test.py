597eb66264b8ad7b19b300ebda9e0229be71480a
south <sharxiouce@163.com>
2017-09-21 15:39:44 +0800

    xxx

--- test.py
+++ test.py
@@ -6,14 +6,19 @@
 #  * @Description: Description
 #  */
 
+from functools import reduce
+
 def f():
 	input_vec = [1,2,3]
 	weights = [4,5,6]
 	
-	test = map(
-				lambda x_w: x_w[1] + 0.1 * x_w[0],
-				zip(input_vec,weights))
-	print(list(test))
+	test = reduce(lambda a, b: a + b,
+				map(lambda x_w: x_w[1] + 0.1 * x_w[0],
+					zip(input_vec,weights))
+				, 0.0)
+	print(test)
 	pass
 if __name__ == '__main__':
 	f()
+
+
